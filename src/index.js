4a4da7b8ed4599b2d261b2370c651d532ff360e3
Gustavo Zimbrón <pptavozl@gmail.com>
2024-10-27 00:23:44 -0600

    trying to load .env file without  extra packages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { handler } from 'HANDLER';
 import { env } from 'ENV';
 import polka from 'polka';
+import { readFileSync, existsSync } from 'fs';
+import { resolve } from 'path';
 
 export const path = env('SOCKET_PATH', false);
 export const host = env('HOST', '0.0.0.0');
@@ -8,8 +10,27 @@ export const port = env('PORT', !path && '3000');
 
 const server = polka().use(handler);
 
+loadEnv();
+
 server.listen({ path, host, port }, () => {
 	console.log(`Listening on ${path ? path : host + ':' + port}`);
 });
+
+function loadEnv() {
+	if (!existsSync('.env')) {
+		return;
+	}
+
+	const envPath = resolve(process.cwd(), '.env');
+	const envData = readFileSync(envPath, 'utf-8');
+	const envVariables = envData.split('\n');
+
+	envVariables.forEach((line) => {
+		const [key, value] = line.split('=');
+		if (key && value && !process.env[key.trim()]) {
+			process.env[key.trim()] = value.trim();
+		}
+	});
+}
 
 export { server };
